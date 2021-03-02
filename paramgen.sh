5507029cc39055485c7af3a0ac759dbfb25a7caf
Maruf Ahmed Bhuiyan <dr.marufahmed89@gmail.com>
2021-03-02 16:06:31 +0600

    Add files via upload

--- paramgen.sh
+++ paramgen.sh
@@ -1,27 +1,61 @@
 #!/usr/bin/bash
+
+info() {
 echo -e "\nPeptide Parameters Generator Version 2.0"
 echo -e "-----Created by Maruf Ahmed Bhuiyan-----\n"
 echo "This script reports the number of aromatic, polar"
 echo -e "non-polar, acidic & basic residues for peptide sequences\n"
+}
 
-set -eu
+# Stop executing the script if nonzero exit status
+# Stop executing the script if any pipe exits wtih nonzero exit status
+
+set -eo pipefail
+
+# Wait for 2 seconds
 sleep 2
 
 i=0
 
+if [[ -z $1 ]]
+then
+	echo "You did not provide any file"
+	echo -e "\nUsage "${0}" <file_name>"
+	exit 1
+fi
+
+if [[ ! -f $1 ]] 
+then
+	echo "File "$1" doesn't exist"
+	exit 1
+fi
+
 while IFS="\n" read p; do
 	aromatic=$(echo $p | tr -cd "F|Y|W" | wc -c)
+	
 	positive=$(echo $p | tr -cd "K|R|H" | wc -c)
+	
 	negative=$(echo $p | tr -cd "D|E" | wc -c)
+	
 	polar=$(echo $p | tr -cd "G|A|P|V|L|I|M" | wc -c)
+	
 	nonpolar=$(echo $p | tr -cd "S|T|C|N|Q" | wc -c)
+	
 	total=$((aromatic+polar+nonpolar+positive+negative))
 
-	if [[ $i -eq 0 ]]; then
-		echo -e "Sequence\tLength\tAromatic\tNegatively Charged\tPositively Charged\tPolar\tNon-polar"
-		((i=i+1))
-	fi
+if [[ $i -eq 0 ]]; then
+echo -e "Sequence\tLength\tAromatic\tNegatively Charged\tPositively Charged\tPolar\tNon-polar"
+((i=i+1))
+fi
 
-	echo -e "$p\t$total\t$aromatic\t$negative\t$positive\t$polar\t$nonpolar"
+echo -e "$p\t$total\t$aromatic\t$negative\t$positive\t$polar\t$nonpolar"
 
-done < "$1" | tee -a peptide_param.tsv
+done < "$1" | tee -a peptide_parameters.tsv
+
+info_end() {
+echo -e "\nScript successfully executed"
+echo -e "\nPeptide parameters were generated in peptide_parameters.tsv file in the current folder"
+}
+
+info
+info_end
