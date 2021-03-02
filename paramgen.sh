8e106a9d6331ecb84013b45a51a36debcd2077f2
Maruf Ahmed Bhuiyan <dr.marufahmed89@gmail.com>
2021-03-02 17:13:30 +0600

    Update paramgen.sh

--- paramgen.sh
+++ paramgen.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
 info() {
 echo -e "\nPeptide Parameters Generator Version 2.0"
@@ -13,6 +13,7 @@ set -eo pipefail
 
 i=0
 
+# Check whether the user provided any file as input
 if [[ -z $1 ]]
 then
 	echo "You did not provide any file"
@@ -20,12 +21,15 @@ then
 	exit 1
 fi
 
+# Check whether the file exists
 if [[ ! -f $1 ]] 
 then
 	echo "File "$1" doesn't exist"
 	exit 1
 fi
 
+# Check whether the file output file already exists.
+# Based on the input append to the existing file or terminate the script
 if [[ -f peptide_parameters.tsv ]]
 then
 	echo -e "\npeptide_parameters.tsv file already exists. "
@@ -48,19 +52,31 @@ then
 	esac
 fi
 
+# read the peptide file line by line 
+# and for each line count the number of aromatic, positive, negative, polar, nonpolar residues
+# and total length of the peptide sequence
+# finally, generate a tab separated value (tsv) file 
 while IFS="\n" read p; do
+	# count the aromatic residues such as Phenylalanine(F), Tyrosine(Y) or Tryptophan(W)
 	aromatic=$(echo $p | tr -cd "F|Y|W" | wc -c)
 	
+	# count the positively charged residues such as Lysine(K), Arginine(R), Histidine(H)
 	positive=$(echo $p | tr -cd "K|R|H" | wc -c)
 	
+	# count the negatively charged resides such as Aspartic acid(D) or Glutamic acid(E)
 	negative=$(echo $p | tr -cd "D|E" | wc -c)
 	
+	# count the polar residues such as Glycine(G), Alanine(A), Proline(P), Valine(V), Leucine(L)
+	# Isoleucine(I), Methionine(M)
 	polar=$(echo $p | tr -cd "G|A|P|V|L|I|M" | wc -c)
 	
+	# count the number of nonpolar residues such as Serine(S), Threonine(T), Cysteine(C), Asparagine(N), Glutamine(Q)
 	nonpolar=$(echo $p | tr -cd "S|T|C|N|Q" | wc -c)
 	
+	# count the length of the peptide sequence
 	total=$((aromatic+polar+nonpolar+positive+negative))
-
+	
+	# print all the residue count in tab separated file
 	if [[ $i -eq 0 ]]; then
 		echo -e "Sequence\tLength\tAromatic\tNegatively Charged\tPositively Charged\tPolar\tNon-polar"
 		((i=i+1))
